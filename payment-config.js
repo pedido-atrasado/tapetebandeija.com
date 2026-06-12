e6ff86f066ee7ffd52634d0a69027a58e789d710
pedido-atrasado <icarobrtv@gmail.com>
2026-06-11 21:54:29 -0300

    Delete payment-config.js

--- payment-config.js
+++
@@ -1,3 +0,0 @@
-// Depois de publicar o backend, informe aqui a URL do Cloudflare Worker.
-// Exemplo: window.PAYMENTS_API_URL = "https://tapetebandeja-pix.seu-usuario.workers.dev";
-window.PAYMENTS_API_URL = "";
